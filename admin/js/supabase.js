a482d5946e981463a4e641ec991f760a645a970c
Sentrum0272 <145539259+Sentrum0272@users.noreply.github.com>
2026-04-05 21:52:38 +0800

    Update supabase.js

--- admin/js/supabase.js
+++ admin/js/supabase.js
@@ -1,8 +1,7 @@
 const SUPABASE_URL = "https://ovjgdrqmervbbcbrposl.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92amdkcnFtZXJ2YmJjYnJwb3NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzODU3NzQsImV4cCI6MjA5MDk2MTc3NH0.nv9X7zyrPxNC2llGQtCiH0KtoDcBIxWSHO3ukzb9OSU";
-
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92amdkcnFtZXJ2YmJjYnJwb3NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzODU3NzQsImV4cCI6MjA5MDk2MTc3NH0.nv9X7zyrPxNC2llGQtCiH0KtoDcBIxWSHO3ukzb9OSU";
 
 window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
-)
+);
