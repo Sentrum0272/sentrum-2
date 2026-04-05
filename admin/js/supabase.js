7fbbc15874c280fe48924e1e1802a30277d245d9
Sentrum0272 <145539259+Sentrum0272@users.noreply.github.com>
2026-04-05 20:57:11 +0800

    Create supabase.js

---
+++ admin/js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://ovjgdrqmervbbcbrposl.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92amdkcnFtZXJ2YmJjYnJwb3NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzODU3NzQsImV4cCI6MjA5MDk2MTc3NH0.nv9X7zyrPxNC2llGQtCiH0KtoDcBIxWSHO3ukzb9OSU";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
